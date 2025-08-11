89d0d6851b728636870e4e22e08a9a2ef0da0d5f
rendichpras <rendichpras@gmail.com>
2025-08-11 18:56:45 +0700

    Tambahkan konfigurasi API untuk meningkatkan batas ukuran body dan respons menjadi 500mb di next.config.ts.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  api: {
+    bodyParser: {
+      sizeLimit: '500mb'
+    },
+    responseLimit: '500mb'
+  }
 };
 
 export default nextConfig;
