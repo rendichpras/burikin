77205bbb399a28e6d3b8cec185e8d83bb766ed4b
rendichpras <rendichpras@gmail.com>
2025-08-11 18:59:16 +0700

    Ubah konfigurasi batas ukuran body API menjadi batas ukuran serverActions di next.config.ts untuk meningkatkan pengelolaan ukuran permintaan.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  api: {
-    bodyParser: {
-      sizeLimit: '500mb'
-    },
-    responseLimit: '500mb'
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '500mb'
+    }
   }
 };
 
